4e8c3135d3cbf0be6deaf17b4b9d511cec250926
DialogIntelligens <team@dialogintelligens.dk>
2024-10-31 15:10:22 +0100

    pinecone

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// Other imports
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { Pool } = require('pg');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { Configuration, OpenAIApi } = require('openai'); // Correct import
-const { PineconeClient } = require('@pinecone-database/pinecone');
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { Pool } from 'pg';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { Configuration, OpenAIApi } from 'openai';
+import { PineconeClient } from '@pinecone-database/pinecone';
+
 
 // Use environment variables for sensitive information
 const SECRET_KEY = process.env.SECRET_KEY || 'Megtigemaskiner00!';
@@ -22,17 +22,15 @@ app.use(cors());
 console.log('Configuration:', Configuration);
 console.log('OpenAIApi:', OpenAIApi);
 
-
 async function generateEmbedding(text, openaiApiKey) {
   const configuration = new Configuration({ apiKey: openaiApiKey });
   const openaiApi = new OpenAIApi(configuration);
   const response = await openaiApi.createEmbedding({
-    model: 'text-embedding-3-large', // Updated model name
+    model: 'text-embedding-3-large',
     input: text,
   });
   return response.data.data[0].embedding;
 }
-
 
 
 app.post('/pinecone-data', authenticateToken, async (req, res) => {
